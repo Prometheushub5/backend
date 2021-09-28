d341ef7b3e811a4ff862588fd9fa2997756e1677
Tozoni <fabiotozoni@gmail.com>
2021-09-28 11:10:24 -0300

    primeira versão da documentação com swagger

--- build/app.js
+++ build/app.js
@@ -1,6 +1,11 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _router = require('./router'); var _router2 = _interopRequireDefault(_router);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _compression = require('compression'); var _compression2 = _interopRequireDefault(_compression);
+var _swaggeruiexpress = require('swagger-ui-express'); var _swaggeruiexpress2 = _interopRequireDefault(_swaggeruiexpress);
+var _swagger_outputjson = require('./swagger_output.json'); var _swagger_outputjson2 = _interopRequireDefault(_swagger_outputjson);
 require('./database');
+
 
 
 
@@ -12,7 +17,10 @@ class App{
     }
 
     middlewares(){
-        this.server.use(_express2.default.json())
+        this.server.use(_express2.default.json());
+        this.server.use(_compression2.default.call(void 0, ));
+        this.server.use(_cors2.default.call(void 0, ))
+        this.server.use('/doc', _swaggeruiexpress2.default.serve, _swaggeruiexpress2.default.setup(_swagger_outputjson2.default))
     }
 
     routes(){
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import routes from './router';
 import cors from 'cors';
 import compression from 'compression';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger_output.json';
 import './database';
 
 
@@ -18,6 +20,7 @@ class App{
         this.server.use(express.json());
         this.server.use(compression());
         this.server.use(cors())
+        this.server.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
     }
 
     routes(){
