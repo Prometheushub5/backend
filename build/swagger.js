4a0bea0463d92e61a608fba77d2d13273ffb0bb8
Tozoni <fabiotozoni@gmail.com>
2021-09-29 00:21:03 -0300

    Revert "primeira versão da documentação com swagger2"
    This reverts commit d8b49b3fd6c615b271ffe46cb8aeb09f7bf3aa19.

--- build/swagger.js
+++
@@ -1,7 +0,0 @@
-"use strict";const swaggerAutogen = require('swagger-autogen')()
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./build/router.js']
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./app.js')
-})
--- src/swagger.js
+++
@@ -1,7 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')()
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./build/router.js']
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./app.js')
-})
