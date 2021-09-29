b4496e6a53e963d678e2f4aa54dec44185874524
Tozoni <fabiotozoni@gmail.com>
2021-09-29 00:30:19 -0300

    testando deploy

--- build/config/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});require ('dotenv/config')
-exports. default = {
-  secret:process.env.AUTH_SECRET,
-  expiresIn:process.env.AUTH_EXPIRE
-}
