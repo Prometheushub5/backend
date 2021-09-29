7148f0ae0c101d91979f43238b0624fe9b86fff5
Tozoni <fabiotozoni@gmail.com>
2021-09-29 00:44:30 -0300

    renomeando rotas

--- src/router.js
+++ src/router.js
@@ -6,16 +6,16 @@ import ControleSessao from './app/controllers/ControleSessao';
 import Porteiro from './app/middeware/auth';
 
 const rotas = new Router();
-rotas.post('/v1.0/clientes/criar/',ControleCliente.criar); //criar clientes
-rotas.get('/v1.0/cursos/consulta/',ControleCurso.listar); //listar cursos
-rotas.post('/login', ControleSessao.verificar); //login
+rotas.post('/v1.0/clientes/',ControleCliente.criar); //criar clientes
+rotas.get('/v1.0/cursos/',ControleCurso.listar); //listar cursos
+rotas.post('/auth', ControleSessao.verificar); //login
 rotas.use(Porteiro) //apartir daqui somente logado
-rotas.post('/v1.0/cursos/criar/',ControleCurso.criar); //criar cursos
-rotas.put('/v1.0/cursos/update/',ControleCurso.update); //update cursos
-rotas.post('/v1.0/consultores/criar/', ControleConsultor.criar); //criar consultores
-rotas.put('/v1.0/consutores/update/',ControleConsultor.update); //update de senha e/ou email de consultor
-rotas.get('/v1.0/clientes/consulta/',ControleCliente.listar); //LISTA CLIENTES POR ID, STATUS OU TODOS
-rotas.put('/v1.0/clientes/atendimento/',ControleCliente.atendimento)
+rotas.post('/v1.0/cursos/',ControleCurso.criar); //criar cursos
+rotas.put('/v1.0/cursos/',ControleCurso.update); //update cursos
+rotas.post('/v1.0/consultores/', ControleConsultor.criar); //criar consultores
+rotas.put('/v1.0/consutores/',ControleConsultor.update); //update de senha e/ou email de consultor
+rotas.get('/v1.0/clientes/',ControleCliente.listar); //LISTA CLIENTES POR ID, STATUS OU TODOS
+rotas.put('/v1.0/clientes/',ControleCliente.atendimento)
 
 
 
